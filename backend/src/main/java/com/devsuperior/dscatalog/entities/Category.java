cf21539e6d15bb5f8950159c7294e70938a11b63
Kaique Gomes <kaiquemedia@outlook.com>
2026-01-20 20:23:27 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
